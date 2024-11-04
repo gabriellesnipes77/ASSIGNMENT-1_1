fcd72224a43b391d5612418ebb610a31d11d2485
BIG DON <BIG DON@MSI>
2024-11-04 15:18:36 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace CCAD16_DEMO_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter age:");
+            double age = Convert.ToDouble(Console.ReadLine());  
+            Console.WriteLine("Enter address:");
+            string address = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("My name is " + name);
+            Console.WriteLine("I am " + age);  
+            Console.WriteLine("I live in " + address);
+        }
+     }
+}
